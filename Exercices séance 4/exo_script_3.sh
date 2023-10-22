b14ac479da926c5266c06df5feef48f4e03f62a7
Solomiia KOROL <107849917+Elesionore@users.noreply.github.com>
2023-10-22 22:11:03 +0200

    typo

--- Exercices séance 4/exo_script_3.sh
+++ Exercices séance 4/exo_script_3.sh
@@ -4,4 +4,4 @@ YEAR="$1"
 MONTH="$2"
 LOCATION="$3"
 
-cat /home/miya/Downloads_Linux/Fichiers/$YEAR"_"$MONTH*.ann | grep Location | cut -f 3 | sort |uniq -c | sort -nr 
+cat /home/miya/Downloads_Linux/Fichiers/$YEAR"_"$MONTH*.ann | grep Location | cut -f 3 | sort |uniq -c | sort -nr | grep $LOCATION
