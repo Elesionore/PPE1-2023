06d2abf3bf2c167f8e81116d6ccdf6a6e7d5e1e4
Solomiia KOROL <107849917+Elesionore@users.noreply.github.com>
2023-11-03 21:41:48 +0100

    Add files via upload

---
+++ miniprojet/programmes/miniprojet2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+filepath=$1
+
+if [ $# -ne 1 ]
+then 
+    echo "pas bon argument, entrer le nom d'un fichier" 
+    exit
+else 
+    if [ -f "$1" ]
+    then 
+        echo "on a bien un fichier"
+    else 
+        echo "on attend un fichier qui existe"
+        exit
+    fi
+fi
+
+A=1
+
+while read -r line
+do
+    if curl -sI $line | egrep  "charset="
+    then 
+        encodage=$(curl -sI $line | egrep -q "charset=")
+	    echo "$A    ${line} ${encodage}"
+    else
+        echo "$A    ${line} pas d'encodage ici"
+    fi
+    A=$(expr $A + 1)
+done < "$1"
